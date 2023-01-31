adb75e1111594b9cfc7e8ab9f08dbe3d9f69c602
0qy <fordnicholasjones@gmail.com>
2023-01-31 15:33:23 +1300

    succesfully check player status for each video

--- client/components/GetVid.jsx
+++ client/components/GetVid.jsx
@@ -9,7 +9,6 @@ function GetVid () {
   const [vidId, setVidId] = useState([])
   const [toggle, setToggle] = useState(true)
   const [videos, setVideos] = useState([]) 
-  
   const [player, setPlayer] = useState(null)
   const [modalOpen, setModalOpen] = useState(true)
   const [loading, setLoading] = useState(true)
@@ -17,16 +16,16 @@ function GetVid () {
   const { playerToggle, setPlayerToggle } = useToggle()
 
   useEffect(() => {
-    search()
+    search(0)
   }, [toggle])
 
-  function search() {
+  function search(index) {
     setTimeout(async() => {
       setVideos(await getMongoData())
     }, 2000)
 
     if (videos.length != undefined && videos.length > 0) {
-      const video = videos[0].yt_id
+      const video = videos[index].yt_id
 
       // Development
       getVidStats(video)
@@ -41,30 +40,39 @@ function GetVid () {
       })
       setVidId(video)
       deleteMongoData(video)
-
-
+      checkState()
+      
+      
       // Production
       // getVidStats(video)
       // .then((data) => {
       //   data.items.map((x) => {
-      //     console.log('viewCount: ', x.statistics.viewCount)
-      //     const vc = x.statistics.viewCount
-      //     vc === 0 
-      //     ? setVidId(video) && deleteMongoData(video)
-      //     : deleteMongoData(video) && search()
-      //   })
-      // })
-      
+        //     console.log('viewCount: ', x.statistics.viewCount)
+        //     const vc = x.statistics.viewCount
+        //     vc === 0 
+        //     ? setVidId(video) && deleteMongoData(video)
+        //     : deleteMongoData(video) && search(0)
+        //   })
+        // })
+        
+      }
+      else {
+        console.log('waiting for data...')
+      }
     }
-    else {
-      console.log('waiting for data...')
-    }
-  }
-  //  check that the video still has 0 views before passing it to the player
-  //  if it fails the check, delete the video and run the same check on the next video in the array
-  //  repeat until one passes and then hand it to the player
 
-  //  check that a video is region locked
+    function checkState() {
+      const playerState = player.getPlayerState()
+      console.log('player state: ', playerState)
+      playerState === -1 
+      ? search(1) && console.log('video skipped!')
+      : console.log('video is playable!')
+    }
+    //  check that the video still has 0 views before passing it to the player
+    //  if it fails the check, delete the video and run the same check on the next video in the array
+    //  repeat until one passes and then hand it to the player
+    
+    //  check that a video is region locked
   //  if it is, remove it from the database
 
   //  Check the .env mode (production || development) and then pass the appropriate table data 
@@ -73,7 +81,7 @@ function GetVid () {
   const stringWidth = window.innerWidth.toString()
   
   function close () {
-    search()
+    search(0)
     setModalOpen(false)
     setPlayerToggle(true)
     player.playVideo()
@@ -95,7 +103,8 @@ function GetVid () {
     setTimeout(() => { setLoading(false) }, 3000)
   }
 
-  function onPlay () {
+
+  function onPlay () { 
     player.unMute()
   }
 
